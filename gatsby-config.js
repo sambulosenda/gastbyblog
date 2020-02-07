61b0cb5062497daa6662cfdad34ef6e97ae79147
sambulo senda <sambulosendas@gmail.com>
2020-02-07 13:07:54 +0000

    My

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Sambulo Blog`,
+    author: `Sambulo Senda`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `SambuloSenda`
     },
   },
   plugins: [
